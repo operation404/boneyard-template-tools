6d64a7510e5676210d8fd51ff0639cc725db56ff
operation404 <operation404@gmail.com>
2023-08-29 11:17:56 -0700

    Adding targeting modes to global boneyard object

--- setup.js
+++ setup.js
@@ -1,8 +1,13 @@
+import * as CONST from './scripts/constants.js';
 import { registerSettings } from './scripts/settings.js';
 import { ByMeasuredTemplateDocument } from './scripts/core classes/ByMeasuredTemplateDocument.js';
 import { BySimpleTokenDocument } from './scripts/core classes/ByTokenDocument.js';
 
 Hooks.once('init', () => {
+    window.Boneyard = window.Boneyard ?? {};
+    window.Boneyard.TemplateTools = {
+        targetingModes: CONST.TARGETING_MODE,
+    };
     registerSettings();
     ByMeasuredTemplateDocument._init();
     BySimpleTokenDocument._init();
