f387121012f55646cfcbb90770d8a9e4f8a6f5f2
operation404 <operation404@gmail.com>
2023-10-23 13:52:46 -0700

    Removing 5e specific functionality, refactoring, working on additional controls

--- scripts/core classes/ByPreviewMeasuredTemplate.js
+++ scripts/core classes/ByPreviewMeasuredTemplate.js
@@ -437,13 +437,12 @@ export class PreviewTemplate extends MeasuredTemplate {
             // Preview specific defaults
             tag: 'previewTemplate',
             interval: 2,
-            lockSize: true,
             lockPosition: false,
+            lockSize: true,
+            lockRotation: false,
             rememberControlled: false,
 
             // Measured template document defaults
-            //x: 0,
-            //y: 0,
             direction: 0,
             distance: canvas.dimensions.distance,
             width: canvas.dimensions.distance,
@@ -455,8 +454,9 @@ export class PreviewTemplate extends MeasuredTemplate {
 
     /**
      *
-     * @param {*} templateData
-     * @param {*} handlers
+     * @param {Object} templateData
+     * @param {Object} callbacks
+     * @returns {PreviewTemplate|null}
      */
     static async createPreview(templateData, callbacks) {
         if (!templateData.hasOwnProperty('t')) return null;
@@ -471,10 +471,7 @@ export class PreviewTemplate extends MeasuredTemplate {
         }
 
         // Remember controlled tokens to restore control after preview.
-        let controlled = [];
-        if (templateData.rememberControlled) {
-            controlled = canvas.tokens.controlled;
-        }
+        const controlled = templateData.rememberControlled ? canvas.tokens.controlled : [];
 
         const cls = CONFIG.MeasuredTemplate.documentClass;
         const templateDoc = new cls(templateData, { parent: canvas.scene });
@@ -482,92 +479,35 @@ export class PreviewTemplate extends MeasuredTemplate {
 
         await templateObj.drawPreview();
 
-        for (const token of controlled) {
-            token.control({ releaseOthers: false });
-        }
+        controlled.forEach((token) => token.control({ releaseOthers: false }));
 
         return templateObj;
     }
-
-    /**
-     * A factory method to create an AbilityTemplate instance using provided data from an Item5e instance
-     * @param {Item5e} item               The Item object for which to construct the template
-     * @returns {AbilityTemplate|null}    The template object, or null if the item does not produce a template
-     */
-    static fromItem(item) {
-        const target = item.system.target ?? {};
-        const templateShape = dnd5e.config.areaTargetTypes[target.type]?.template;
-        if (!templateShape) return null;
-
-        // Prepare template data
-        const templateData = {
-            t: templateShape,
-            user: game.user.id,
-            distance: target.value,
-            direction: 0,
-            x: 0,
-            y: 0,
-            fillColor: game.user.color,
-            flags: { dnd5e: { origin: item.uuid } },
-        };
-
-        // Additional type-specific data
-        switch (templateShape) {
-            case 'cone':
-                templateData.angle = CONFIG.MeasuredTemplate.defaults.angle;
-                break;
-            case 'rect': // 5e rectangular AoEs are always cubes
-                templateData.distance = Math.hypot(target.value, target.value);
-                templateData.width = target.value;
-                templateData.direction = 45;
-                break;
-            case 'ray': // 5e rays are most commonly 1 square (5 ft) in width
-                templateData.width = target.width ?? canvas.dimensions.distance;
-                break;
-            default:
-                break;
-        }
-
-        // Return the template constructed from the item data
-        const cls = CONFIG.MeasuredTemplate.documentClass;
-        const template = new cls(templateData, { parent: canvas.scene });
-        const object = new this(template);
-        object.item = item;
-        object.actorSheet = item.actor?.sheet || null;
-        return object;
-    }
-
-    /* -------------------------------------------- */
 
     /**
      * Creates a preview of the spell template.
      * @returns {Promise}  A promise that resolves with the final measured template if created.
      */
     drawPreview() {
-        const initialLayer = canvas.activeLayer;
+        this.#initialLayer = canvas.activeLayer;
 
         // Draw the template and switch to the template layer
         this.draw();
         this.layer.activate();
         this.layer.preview.addChild(this);
 
-        // Hide the sheet that originated the preview
-        this.actorSheet?.minimize();
-
         // Activate interactivity
-        return this.activatePreviewListeners(initialLayer);
+        return this.activatePreviewListeners();
     }
 
     /* -------------------------------------------- */
 
     /**
      * Activate listeners for the template preview
-     * @param {CanvasLayer} initialLayer  The initially active CanvasLayer to re-activate after the workflow is complete
      * @returns {Promise}                 A promise that resolves with the final measured template if created.
      */
-    activatePreviewListeners(initialLayer) {
+    activatePreviewListeners() {
         return new Promise((resolve, reject) => {
-            this.#initialLayer = initialLayer;
             this.#events = {
                 cancel: this._onCancelPlacement.bind(this),
                 confirm: this._onConfirmPlacement.bind(this),
@@ -609,11 +549,16 @@ export class PreviewTemplate extends MeasuredTemplate {
      */
     _onMovePlacement(event) {
         event.stopPropagation();
+
+        if (this.lockPosition) return;
+
         const now = Date.now(); // Apply a 20ms throttle
         if (now - this.#moveTime <= 20) return;
+
         const center = event.data.getLocalPosition(this.layer);
         const interval = canvas.grid.type === CONST.GRID_TYPES.GRIDLESS ? 0 : 2;
         const snapped = canvas.grid.getSnappedPosition(center.x, center.y, interval);
+
         this.document.updateSource({ x: snapped.x, y: snapped.y });
         this.refresh();
         this.#moveTime = now;
