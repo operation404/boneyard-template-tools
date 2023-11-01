2e38bda8aa5e6b0b50e8d286f71872e919f21f98
operation404 <operation404@gmail.com>
2023-10-31 20:05:11 -0700

    Start work on SavingThrow Action, add optional print flag to 5e Actions. Still need to implement printing

--- scripts/actions/systems/dnd5e.js
+++ scripts/actions/systems/dnd5e.js
@@ -7,8 +7,10 @@ class Damage extends Action {
      * @param {object} data
      * @param {string} data.damageType
      * @param {number} data.value
+     * @param {boolean} [data.print]
      */
     constructor(data) {
+        if (!data.hasOwnProperty('print')) data.print = false;
         super(data);
     }
 
@@ -16,12 +18,15 @@ class Damage extends Action {
      * @param {object} data
      * @param {string} data.damageType
      * @param {number} data.value
+     * @param {boolean} [data.print]
      * @throws 'damageType' invalid.
      * @throws 'value' must be integer.
+     * @throws 'print' must be boolean.
      */
-    static validateData({ damageType, value }) {
-        if (!CONFIG.DND5E.damageTypes[damageType]) throw `'damageType' invalid.`;
+    static validateData({ damageType, value, print }) {
+        if (!CONFIG.DND5E.damageTypes.hasOwnProperty(damageType)) throw `'damageType' invalid.`;
         if (!Number.isInteger(value)) throw `'value' must be integer.`;
+        if (typeof print !== 'boolean') throw `'print' must be boolean.`;
     }
 
     /**
@@ -58,14 +63,22 @@ class Healing extends Damage {
     /**
      * @param {object} data
      * @param {number} data.value
+     * @param {boolean} [data.print]
      */
     constructor(data) {
         super(data);
     }
 
-    /** @override */
+    /**
+     * @param {object} data
+     * @param {number} data.value
+     * @param {boolean} [data.print]
+     * @throws 'value' must be integer.
+     * @throws 'print' must be boolean.
+     */
     static validateData({ value }) {
         if (!Number.isInteger(value)) throw `'value' must be integer.`;
+        if (typeof print !== 'boolean') throw `'print' must be boolean.`;
     }
 
     /** @override */
@@ -74,7 +87,55 @@ class Healing extends Damage {
     }
 }
 
-class SavingThrow extends Action {}
+class SavingThrow extends Action {
+    /**
+     * @param {object} data
+     * @param {string} data.save
+     * @param {number} [data.bonus]
+     * @param {number} data.dc
+     * @param {Action|Action[]} data.passActions
+     * @param {Action|Action[]} [data.failActions]
+     * @param {boolean} [data.print]
+     */
+    constructor(data) {
+        data.passActions = Array.isArray(data.passActions) ? data.passActions : [data.passActions];
+        if (!data.hasOwnProperty('bonus')) data.bonus = 0;
+        if (!data.hasOwnProperty('failActions')) data.failActions = [];
+        data.failActions = Array.isArray(data.failActions) ? data.failActions : [data.failActions];
+        if (!data.hasOwnProperty('print')) data.print = false;
+        super(data);
+    }
+
+    /**
+     * @param {object} data
+     * @param {string} data.save
+     * @param {number} [data.bonus]
+     * @param {number} data.dc
+     * @param {Action[]} data.passActions
+     * @param {Action[]} [data.failActions]
+     * @param {boolean} [data.print]
+     * @throws 'save' invalid.
+     * @throws 'bonus' must be integer.
+     * @throws 'dc' must be integer.
+     * @throws 'passActions' must be instance(s) of Action.
+     * @throws 'failActions' must be instance(s) of Action.
+     * @throws 'print' must be boolean.
+     */
+    static validateData({ save, bonus, dc, passActions, failActions }) {
+        if (!CONFIG.DND5E.abilities.hasOwnProperty(save)) throw `'save' invalid.`;
+        if (!Number.isInteger(bonus)) throw `'bonus' must be integer.`;
+        if (!Number.isInteger(dc)) throw `'dc' must be integer.`;
+        passActions.forEach((a) => {
+            if (!(a instanceof Action)) throw `'passActions' must be instances of Action.`;
+        });
+        failActions.forEach((a) => {
+            if (!(a instanceof Action)) throw `'failActions' must be instances of Action.`;
+        });
+        if (typeof print !== 'boolean') throw `'print' must be boolean.`;
+    }
+
+    static resolve({ save, bonus, dc, passActions, failActions }) {}
+}
 
 class AbilityCheck extends Action {}
 
