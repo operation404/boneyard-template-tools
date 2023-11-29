9415226e4ed3f8ffa6a51b66c2369f46d104d2e3
operation404 <operation404@gmail.com>
2023-11-29 12:26:09 -0800

    Reorganizing, preparing to split preset actions into separate module

--- scripts/core classes/PreviewMeasuredTemplate.js
+++ scripts/core classes/PreviewMeasuredTemplate.js
@@ -88,19 +88,6 @@ export class PreviewTemplate extends MeasuredTemplate {
         };
     }
 
-    /*
-
-    TODO
-
-    Also, for the template itself, the shape is a simple pixi graphics object.
-    If I make a pixi container and put multiple shapes in it, I should be able
-    to make custom composite templates. The container seems to test all children for stuff like
-    a point being inside of it - even if it doesn't, I could easily override the method
-    and make it do that.
-    So like 3 overlapping circles all as one template. 
-
-    */
-
     /**
      *
      * @param {Object} templateData
